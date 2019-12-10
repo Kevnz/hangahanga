f00b992abe7732da62b67ab9488d742eb655745d
Kevin Isom <kevin.isom@gmail.com>
2019-12-10 14:40:39 +1300

    about

---
+++ src/ui/features/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Section, Title, SubTitle, Container } from '@brightleaf/elements'
+import { Link } from 'react-router-dom'
+
+export default () => (
+  <Section>
+    <Title>Hangahanga</Title>
+    <SubTitle>(noun) practice, habit, strategy.</SubTitle>
+    <Container>About</Container>
+  </Section>
+)
